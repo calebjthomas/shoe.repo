694e9e4eb574c3336d42649e6d7e001345280fbb
calebjthomas <69021117+calebjthomas@users.noreply.github.com>
2020-07-30 18:41:20 -0700

    Add files via upload

---
+++ CalculateShoesAdult.java
@@ -0,0 +1,33 @@
+/* This program is to calculate the average prices and different sizes of
+different types of shoes and associate each price to the right shoes.
+*/
+
+class CalculateShoesAdult {
+public static void main(String args[]) {
+	double price[] = { 65, 80, 75, 55, 35 };
+	double average = (65 + 80 + 75 + 55 + 35)/5;
+	double Vans; // Shoe sold at this store
+	Vans = 65; // price of Vans
+	double Nike; // Shoe sold at this store
+	Nike = 80; // price of Nike
+	double Adidas; // Shoe sold at this store
+	Adidas = 75; // price of Adidas
+	double Puma; // Shoe sold at this store
+	Puma = 55; // price of Puma
+	double Fila; // Shoe sold at this store
+	Fila = 35; // price of Fila
+	
+	for(int i=0; i<1; i++) {
+	for(int j=0; j<1; j++) {
+	
+	}
+}
+	System.out.println("The prices of different kinds of shoes are as follows.");
+	System.out.println("Vans are $" + Vans + ".");
+	System.out.println("Nikes are $" + Nike + ".");
+	System.out.println("Adidas are $" + Adidas + ".");
+	System.out.println("Pumas are $" + Puma + ".");
+	System.out.println("Filas are $" + Fila + ".");
+	System.out.println("The average price of the Adult shoes in this store is $" + average + ".");
+}
+}
